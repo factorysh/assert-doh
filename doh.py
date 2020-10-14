15972f2c20fabca2440f74e63309985f4b7aafc9
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-14 17:34:47 +0200

    GET

--- doh.py
+++ doh.py
@@ -1,9 +1,10 @@
 #!/bin/env python3
 
+import os
+from base64 import b64encode
+
 import httpx
 from dns import message, rdatatype
-import os
-
 
 class Resolver:
     def __init__(self, server, client=None):
@@ -12,20 +13,30 @@ class Resolver:
             client = httpx.Client(http2=True)
         self.client = client
 
-    def query(self, queryname, rdtype=rdatatype.ANY):
+    def query(self, queryname, rdtype=rdatatype.ANY, method="POST"):
         q = message.make_query(queryname, rdtype)
-        response = self.client.post(
-            self.server,
-            headers={
-                "Accept": "application/dns-message",
-                "Content-Type": "application/dns-message",
-            },
-            content=q.to_wire(),
-        )
+        assert method in ['GET', 'POST'], "bad method : %s" % method
+        if method == "POST":
+            response = self.client.post(
+                self.server,
+                headers={
+                    "Accept": "application/dns-message",
+                    "Content-Type": "application/dns-message",
+                },
+                content=q.to_wire(),
+            )
+        else:
+            response = self.client.get(
+                "%s?dns=%s" % (self.server, b64encode(q.to_wire()).decode('ASCII')),
+                headers={
+                    "Accept": "application/dns-message",
+                },
+            )
+
         assert response.status_code == 200
         print(response.headers)
         r = message.from_wire(response.content)
-        print(q.id, r.id)
+        #print(q.id, r.id)
         return r
 
 
